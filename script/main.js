949015cbc016632a625ffbf07d76905432d0b73e
Bruno Vigel <brunorosav@gmail.com>
2022-02-08 17:44:50 -0300

    script refactoring

--- script/main.js
+++ script/main.js
@@ -9,51 +9,35 @@ async function setDay() {
 
     // Work Card
     document.querySelector('#work-card .title').innerText = `${dadosJSON[0].title}`
-    const workCurrent = document.querySelector('#work-card .current-hours');
-    const workPrevious = document.querySelector('#work-card .previous-hours');
-
-    workCurrent.innerText = `${dadosJSON[0].timeframes.daily.current}hrs`
-    workPrevious.innerText = `Yesterday - ${dadosJSON[0].timeframes.daily.previous}hrs`
+    document.querySelector('#work-card .current-hours').innerText = `${dadosJSON[0].timeframes.daily.current}hrs`
+    document.querySelector('#work-card .previous-hours').innerText = `Yesterday - ${dadosJSON[0].timeframes.daily.previous}hrs`
 
     // Play Card
     document.querySelector('#play-card .title').innerText = `${dadosJSON[1].title}`
-    const playCurrent = document.querySelector('#play-card .current-hours');
-    const playPrevious = document.querySelector('#play-card .previous-hours')
-
-    playCurrent.innerText = `${dadosJSON[1].timeframes.daily.current}hrs`
-    playPrevious.innerText = `Yesterday - ${dadosJSON[1].timeframes.daily.previous}hrs`
+    document.querySelector('#play-card .current-hours').innerText = `${dadosJSON[1].timeframes.daily.current}hrs`
+    document.querySelector('#play-card .previous-hours').innerText = `Yesterday - ${dadosJSON[1].timeframes.daily.previous}hrs`
 
     // Study Card
     document.querySelector('#study-card .title').innerText = `${dadosJSON[2].title}`
-    const studyCurrent = document.querySelector('#study-card .current-hours');
-    const studyPrevious = document.querySelector('#study-card .previous-hours')
-
-    studyCurrent.innerText = `${dadosJSON[2].timeframes.daily.current}hrs`
-    studyPrevious.innerText = `Yesterday - ${dadosJSON[2].timeframes.daily.previous}hrs`
+    document.querySelector('#study-card .current-hours').innerText = `${dadosJSON[2].timeframes.daily.current}hrs`
+    document.querySelector('#study-card .previous-hours').innerText = `Yesterday - ${dadosJSON[2].timeframes.daily.previous}hrs`
 
     // Exercise Card
     document.querySelector('#exercise-card .title').innerText = `${dadosJSON[03].title}`
-    const exerciseCurrent = document.querySelector('#exercise-card .current-hours');
-    const exercisePrevious = document.querySelector('#exercise-card .previous-hours')
+    document.querySelector('#exercise-card .current-hours').innerText = `${dadosJSON[3].timeframes.daily.current}hrs`
+    document.querySelector('#exercise-card .previous-hours').innerText = `Yesterday - ${dadosJSON[3].timeframes.daily.previous}hrs`
 
-    exerciseCurrent.innerText = `${dadosJSON[3].timeframes.daily.current}hrs`
-    exercisePrevious.innerText = `Yesterday - ${dadosJSON[3].timeframes.daily.previous}hrs`
+ 
 
     // Social Card
     document.querySelector('#social-card .title').innerText = `${dadosJSON[04].title}`
-    const socialCurrent = document.querySelector('#social-card .current-hours');
-    const socialPrevious = document.querySelector('#social-card .previous-hours')
-
-    socialCurrent.innerText = `${dadosJSON[4].timeframes.daily.current}hrs`
-    socialPrevious.innerText = `Yesterday - ${dadosJSON[4].timeframes.daily.previous}hrs`
+    document.querySelector('#social-card .current-hours').innerText = `${dadosJSON[4].timeframes.daily.current}hrs`
+    document.querySelector('#social-card .previous-hours').innerText = `Yesterday - ${dadosJSON[4].timeframes.daily.previous}hrs`
 
      // Self care Card
-    document.querySelector('#self-care-card .title').innerText = `${dadosJSON[5].title}`
-     const selfCareCurrent = document.querySelector('#self-care-card .current-hours');
-     const selfCarePrevious = document.querySelector('#self-care-card .previous-hours')
-
-     selfCareCurrent.innerText = `${dadosJSON[5].timeframes.daily.current}hrs`
-     selfCarePrevious.innerText = `Yesterday - ${dadosJSON[5].timeframes.daily.previous}hrs`
+     document.querySelector('#self-care-card .title').innerText = `${dadosJSON[5].title}`
+     document.querySelector('#self-care-card .current-hours').innerText = `${dadosJSON[5].timeframes.daily.current}hrs`
+     document.querySelector('#self-care-card .previous-hours').innerText = `Yesterday - ${dadosJSON[5].timeframes.daily.previous}hrs`
 }
 
 const btnDay = document.querySelector('[data-time="day"]')
@@ -67,46 +51,29 @@ async function setWeek() {
 
 
     // Work Card
-    const workCurrent = document.querySelector('#work-card .current-hours');
-    const workPrevious = document.querySelector('#work-card .previous-hours')
-
-    workCurrent.innerText = `${dadosJSON[0].timeframes.weekly.current}hrs`
-    workPrevious.innerText = `Last Week - ${dadosJSON[0].timeframes.weekly.previous}hrs`
+    document.querySelector('#work-card .current-hours').innerText = `${dadosJSON[0].timeframes.weekly.current}hrs`
+    document.querySelector('#work-card .previous-hours').innerText = `Last Week - ${dadosJSON[0].timeframes.weekly.previous}hrs`
 
     // Play Card
-    const playCurrent = document.querySelector('#play-card .current-hours');
-    const playPrevious = document.querySelector('#play-card .previous-hours')
-
-    playCurrent.innerText = `${dadosJSON[1].timeframes.weekly.current}hrs`
-    playPrevious.innerText = `Last Week - ${dadosJSON[1].timeframes.weekly.previous}hrs`
+    document.querySelector('#play-card .current-hours').innerText = `${dadosJSON[1].timeframes.weekly.current}hrs`
+    document.querySelector('#play-card .previous-hours').innerText = `Last Week - ${dadosJSON[1].timeframes.weekly.previous}hrs`
 
     // Study Card
-    const studyCurrent = document.querySelector('#study-card .current-hours');
-    const studyPrevious = document.querySelector('#study-card .previous-hours')
+    document.querySelector('#study-card .current-hours').innerText = `${dadosJSON[2].timeframes.weekly.current}hrs`
+    document.querySelector('#study-card .previous-hours').innerText = `Last Week - ${dadosJSON[2].timeframes.weekly.previous}hrs`
 
-    studyCurrent.innerText = `${dadosJSON[2].timeframes.weekly.current}hrs`
-    studyPrevious.innerText = `Last Week - ${dadosJSON[2].timeframes.weekly.previous}hrs`
 
     // Exercise Card
-    const exerciseCurrent = document.querySelector('#exercise-card .current-hours');
-    const exercisePrevious = document.querySelector('#exercise-card .previous-hours')
-
-    exerciseCurrent.innerText = `${dadosJSON[3].timeframes.weekly.current}hrs`
-    exercisePrevious.innerText = `Last Week - ${dadosJSON[3].timeframes.weekly.previous}hrs`
+    document.querySelector('#exercise-card .current-hours').innerText = `${dadosJSON[3].timeframes.weekly.current}hrs`
+    document.querySelector('#exercise-card .previous-hours').innerText = `Last Week - ${dadosJSON[3].timeframes.weekly.previous}hrs`
 
     // Social Card
-    const socialCurrent = document.querySelector('#social-card .current-hours');
-    const socialPrevious = document.querySelector('#social-card .previous-hours')
-
-    socialCurrent.innerText = `${dadosJSON[4].timeframes.weekly.current}hrs`
-    socialPrevious.innerText = `Last Week - ${dadosJSON[4].timeframes.weekly.previous}hrs`
+    document.querySelector('#social-card .current-hours').innerText = `${dadosJSON[4].timeframes.weekly.current}hrs`
+    document.querySelector('#social-card .previous-hours').innerText = `Last Week - ${dadosJSON[4].timeframes.weekly.previous}hrs`
 
      // Self care Card
-     const selfCareCurrent = document.querySelector('#self-care-card .current-hours');
-     const selfCarePrevious = document.querySelector('#self-care-card .previous-hours')
-
-     selfCareCurrent.innerText = `${dadosJSON[5].timeframes.weekly.current}hrs`
-     selfCarePrevious.innerText = `Last Week - ${dadosJSON[5].timeframes.weekly.previous}hrs`
+     document.querySelector('#self-care-card .current-hours').innerText = `${dadosJSON[5].timeframes.weekly.current}hrs`
+     document.querySelector('#self-care-card .previous-hours').innerText = `Last Week - ${dadosJSON[5].timeframes.weekly.previous}hrs`
 }
 
 const btnWeek = document.querySelector('[data-time="week"]')
@@ -120,46 +87,30 @@ async function setMonth() {
 
 
     // Work Card
-    const workCurrent = document.querySelector('#work-card .current-hours');
-    const workPrevious = document.querySelector('#work-card .previous-hours')
-
-    workCurrent.innerText = `${dadosJSON[0].timeframes.monthly.current}hrs`
-    workPrevious.innerText = `Last Month - ${dadosJSON[0].timeframes.monthly.previous}hrs`
+    document.querySelector('#work-card .current-hours').innerText = `${dadosJSON[0].timeframes.monthly.current}hrs`
+    document.querySelector('#work-card .previous-hours').innerText = `Last Month - ${dadosJSON[0].timeframes.monthly.previous}hrs`
 
     // Play Card
-    const playCurrent = document.querySelector('#play-card .current-hours');
-    const playPrevious = document.querySelector('#play-card .previous-hours')
-
-    playCurrent.innerText = `${dadosJSON[1].timeframes.monthly.current}hrs`
-    playPrevious.innerText = `Last Month - ${dadosJSON[1].timeframes.monthly.previous}hrs`
+    document.querySelector('#play-card .current-hours').innerText = `${dadosJSON[1].timeframes.monthly.current}hrs`
+    document.querySelector('#play-card .previous-hours').innerText = `Last Month - ${dadosJSON[1].timeframes.monthly.previous}hrs`
 
     // Study Card
-    const studyCurrent = document.querySelector('#study-card .current-hours');
-    const studyPrevious = document.querySelector('#study-card .previous-hours')
-
-    studyCurrent.innerText = `${dadosJSON[2].timeframes.monthly.current}hrs`
-    studyPrevious.innerText = `Last Month - ${dadosJSON[2].timeframes.monthly.previous}hrs`
+    document.querySelector('#study-card .current-hours').innerText = `${dadosJSON[2].timeframes.monthly.current}hrs`
+    document.querySelector('#study-card .previous-hours').innerText = `Last Month - ${dadosJSON[2].timeframes.monthly.previous}hrs`
 
     // Exercise Card
-    const exerciseCurrent = document.querySelector('#exercise-card .current-hours');
-    const exercisePrevious = document.querySelector('#exercise-card .previous-hours')
-
-    exerciseCurrent.innerText = `${dadosJSON[3].timeframes.monthly.current}hrs`
-    exercisePrevious.innerText = `Last Month - ${dadosJSON[3].timeframes.monthly.previous}hrs`
+    document.querySelector('#exercise-card .current-hours').innerText = `${dadosJSON[3].timeframes.monthly.current}hrs`
+    document.querySelector('#exercise-card .previous-hours').innerText = `Last Month - ${dadosJSON[3].timeframes.monthly.previous}hrs`
 
     // Social Card
-    const socialCurrent = document.querySelector('#social-card .current-hours');
-    const socialPrevious = document.querySelector('#social-card .previous-hours')
-
-    socialCurrent.innerText = `${dadosJSON[4].timeframes.monthly.current}hrs`
-    socialPrevious.innerText = `Last Month - ${dadosJSON[4].timeframes.monthly.previous}hrs`
+    const socialCurrent = document.querySelector('#social-card .current-hours').innerText = `${dadosJSON[4].timeframes.monthly.current}hrs`
+    const socialPrevious = document.querySelector('#social-card .previous-hours').innerText = `Last Month - ${dadosJSON[4].timeframes.monthly.previous}hrs`
 
      // Self care Card
-     const selfCareCurrent = document.querySelector('#self-care-card .current-hours');
-     const selfCarePrevious = document.querySelector('#self-care-card .previous-hours')
+    document.querySelector('#self-care-card .current-hours').innerText = `${dadosJSON[5].timeframes.monthly.current}hrs`
+    document.querySelector('#self-care-card .previous-hours').innerText = `Last Month - ${dadosJSON[5].timeframes.monthly.previous}hrs`
 
-     selfCareCurrent.innerText = `${dadosJSON[5].timeframes.monthly.current}hrs`
-     selfCarePrevious.innerText = `Last Month - ${dadosJSON[5].timeframes.monthly.previous}hrs`
+
 }
 
 const btnMonth = document.querySelector('[data-time="month"]')
